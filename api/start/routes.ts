44b308558d71290a04713a67f6fadd548aa55a52
Jonatas Da Silva <jonatas.publinet@gmail.com>
2021-01-23 12:11:06 -0300

    error

--- api/start/routes.ts
+++ api/start/routes.ts
@@ -1,33 +1,13 @@
-module.exports = (app: any) => {
-  app.route("/")
-    .get((req: any, res: any) => res.json({"success": "Sucesso"}))
+import {Router} from 'express';
+ 
+const routers = Router();
 
-  app.route("/clients")
-    .get(app.controllers.api.clients.get)
-    .post(app.controllers.clients.post)
-  
-  app.route("/categories")
-  .get(app.controllers.api.categories.get)
-  .post(app.controllers.categories.post)
+import clientRouter from '../app/controllers/clients.routers';
 
-  app.route("/products")
-  .get(app.controllers.api.products.get)
-  .post(app.controllers.products.post)
+routers.get('/', (req, res) => {
+  return req.json("Sucesso");
+});
 
-  app.route("/products/categoteries/:id")
-  .get(app.controllers.api.products.get)
-  
-  app.route("/sales_order")
-  .get(app.controllers.api.sales_order.get)
-  .delete(app.controllers.sales_order.delete)
+routers.get('/clients', clientRouter);
 
-  app.route("/sales_order_item")
-  .get(app.controllers.api.sales_order_item.get)
-  .delete(app.controllers.sales_order_item.delete)
-
-  app.route("/adresses")
-    .get(app.controllers.api.adresses.get)
-
-  app.route("/contacts")
-  .get(app.controllers.api.contacts.get)
-}
+export default routers;
